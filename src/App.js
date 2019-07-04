d405bc754ac2da5949d7ead36518914ce9566d2f
ghivalla <ghivalla@gmail.com>
2019-07-04 19:20:05 +0200

    update the menu-items to navigate withRouter HighOrderComponent

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import React from 'react';
 import HomePage from './components/pages/homepage/homepage.component';
 import './App.css';
+import {Route,Switch} from 'react-router-dom';
+const PedalsPage = () =>(
+	<div>
+		<h1>PEDALS PAGE </h1>
+	</div>
+);
 
 function App() {
   return (
-    <div><HomePage/></div>
+    <div>
+    	<Switch>
+    		<Route exact path="/" component={HomePage}/>
+    		<Route path="/pedals" component={PedalsPage} />
+    	</Switch>
+    </div>
   );
 }
 
--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -10,29 +10,34 @@ class Directory extends Component{
         {
           title: 'Pedals',
           imageUrl: 'https://cdn.pixabay.com/photo/2016/08/15/16/56/guitar-1595862_960_720.jpg',
-          id: 1
+          id: 1,
+          linkUrl:'pedals'
         },
         {
           title: 'Amps',
           imageUrl: 'https://cdn.pixabay.com/photo/2018/09/02/15/31/guitar-amplifier-3649042_960_720.jpg',
-          id: 2
+          id: 2,
+          linkUrl:''
         },
         {
           title: 'Accessories',
           imageUrl: 'https://cdn.pixabay.com/photo/2016/08/12/12/51/music-accessories-1588402_960_720.jpg',
-          id: 3
+          id: 3,
+          linkUrl:''
         },
         {
           title: 'Guitars',
           imageUrl: 'https://images.pexels.com/photos/165971/pexels-photo-165971.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
           size: 'large',
-          id: 4
+          id: 4,
+          linkUrl:''
         },
         {
           title: 'Basses',
           imageUrl: 'https://cdn.pixabay.com/photo/2015/10/20/07/34/bass-997349_960_720.jpg',
           size: 'large',
-          id: 5
+          id: 5,
+          linkUrl:''
         }
       ]
 		}
@@ -41,8 +46,8 @@ class Directory extends Component{
 		return(
 			<div className='directory-menu'>
 				{
-					this.state.sections.map(({title ,imageUrl, id, size})=>(
-						<MenuItem key={id} imageUrl={imageUrl} title={title} size={size}/>
+					this.state.sections.map(({id, ...otherSectionProps})=>(
+						<MenuItem key={id} {...otherSectionProps}/>
 					))
 				}
 			</div>
